b3aba18ee2b0f69060003f181468d7fdb0f41b10
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-03-01 22:23:48 +0100

    Initial commit

---
+++ src/ObservablePipelines/IObservablePipe.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ObservablePipelines
+{
+    public interface IPipe<TIn, TOut>
+    {
+        IObservable<TOut> Handle(IObservable<TIn> source);
+    }
+
+    public interface IPipelineBuilder
+    {
+        IPipe<TIn, TOut> Build<TIn, TOut>();
+    }
+
+    public interface IPipelineConfigurationBuilder
+    {
+        IPipelineConfigurationBuilder Add<TConfiguration>(TConfiguration configuration);
+    }
+}
