ec6f69c7aec229616c6343fdc780dfbd0654d882
Tobias Streng <streng@PZS.de>
2022-03-04 19:24:54 +0100

    :zap: Add Pipe Extension

---
+++ src/ObservablePipelines/Extensions/ObservableExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObservablePipelines.Extensions
+{
+    public static class ObservableExtensions
+    {
+        public static IObservable<TOut> Pipe<TIn, TOut>(
+            this IObservable<TIn> source,
+            IPipe<TIn, TOut> pipe
+        ) => pipe.Handle(source);
+    }
+}
