e33528f9f7737b2e89e0b20d0d9c2b38140160f3
Tobias Streng <streng@PZS.de>
2022-03-04 19:31:16 +0100

    :art: Add ServiceCollection Clone() method

---
+++ src/ObservablePipelines/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace ObservablePipelines.Extensions
+{
+    internal static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection Clone(this IServiceCollection services) {
+            var newServiceCollection = new ServiceCollection();
+
+            foreach (var service in services) {
+                newServiceCollection.Add(service);
+            }
+
+            return newServiceCollection;
+        }
+    }
+}
