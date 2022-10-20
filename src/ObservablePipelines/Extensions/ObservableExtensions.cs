01d85a2198a17b73c4c871da4009d962e9e2d2ab
Tobias Streng <54748454+TobiStr@users.noreply.github.com>
2022-10-20 10:16:22 +0200

    :rocket: Add Lambda Pipe Extension

--- src/ObservablePipelines/Extensions/ObservableExtensions.cs
+++ src/ObservablePipelines/Extensions/ObservableExtensions.cs
@@ -8,5 +8,10 @@ namespace ObservablePipelines.Extensions
             this IObservable<TIn> source,
             IPipe<TIn, TOut> pipe
         ) => pipe.Handle(source);
+
+        public static IObservable<TOut> Pipe<TIn, TOut>(
+            this IObservable<TIn> source,
+            Func<IObservable<TIn>, IObservable<TOut>> pipe
+        ) => pipe(source);
     }
 }
